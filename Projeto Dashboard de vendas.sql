8dfdebf13a95a906d16bfda9acfaa6070cfc7480
José Augusto Soares <115514263+joseaugusto-olliver@users.noreply.github.com>
2025-05-22 10:11:01 -0300

    Add files via upload
    finalizado

--- Projeto Dashboard de vendas.sql
+++ Projeto Dashboard de vendas.sql
@@ -58,10 +58,52 @@ limit 5
 -- (Query 3) Marcas que mais venderam no mês
 -- Colunas: marca, vendas (#)
 
+select 
+	pro.brand as marca,
+	count(fun.paid_date) as "vendas (#)"
+from sales.products as pro
+left join sales.funnel as fun
+	on pro.product_id = fun.product_id
+where paid_date between '2021-08-01' and '2021-08-31'
+group by pro.brand
+order by "vendas (#)" desc
+limit 5
+
 
 -- (Query 4) Lojas que mais venderam
 -- Colunas: loja, vendas (#)
 
+select 
+	sto.store_name as loja,
+	count(fun.paid_date) as "vendas (#)"
+from sales.funnel as  fun
+left join sales.stores as sto
+	on fun.store_id = sto.store_id
+where paid_date between '2021-08-01' and '2021-08-31'
+group by loja
+order by "vendas (#)" desc
+limit 5
+
 
 -- (Query 5) Dias da semana com maior número de visitas ao site
 -- Colunas: dia_semana, dia da semana, visitas (#)
+
+select 
+	extract('dow' from visit_page_date) as dia_semana,
+	case 
+		when extract('dow' from visit_page_date)=0 then 'Domingo'
+		when extract('dow' from visit_page_date)=1 then 'Segunda'
+		when extract('dow' from visit_page_date)=2 then 'Terça'
+		when extract('dow' from visit_page_date)=3 then 'Quarta'
+		when extract('dow' from visit_page_date)=4 then 'Quinta'
+		when extract('dow' from visit_page_date)=5 then 'Sexta'
+		when extract('dow' from visit_page_date)=6 then 'Sábado' 
+	else null end as "dia da semana",
+	count(*) as "visitas (#)"
+from sales.funnel
+where visit_page_date between '2021-08-01' and '2021-08-31'
+group by dia_semana
+order by "visitas (#)" desc
+
+
+
